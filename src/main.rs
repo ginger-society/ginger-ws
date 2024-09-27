b83b2b50744c6e169f92db5859e1648aa19305ef
Ginger Society Admin <admin@gingersociety.org>
2024-09-27 17:09:11 +0530

    feat: updated k8 command and ampq uri env key

--- src/main.rs
+++ src/main.rs
@@ -202,7 +202,7 @@ async fn user_authenticated(
 }
 
 async fn connect_rabbitmq() -> Result<RabbitChannel, lapin::Error> {
-    let addr = std::env::var("RABBITMQ_URL")
+    let addr = std::env::var("AMPQ_URI")
         .unwrap_or_else(|_| "amqp://user:password@localhost:5672/%2f".to_string());
 
     let conn = Connection::connect(&addr, ConnectionProperties::default()).await?;
